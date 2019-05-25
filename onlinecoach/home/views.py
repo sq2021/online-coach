cfc147f2e7bdd574eb16561aa64d02e2808b98df
TopKeingt <ppkhiemnguyen@gmail.com>
2019-05-25 18:13:50 -0400

    add main page

--- onlinecoach/home/views.py
+++ onlinecoach/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from qa.views import Question
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    questions = Question.objects.all()
+    context = {
+        'latest_questions_list': questions
+    }
+    return render(request, 'home/index.html', context)
