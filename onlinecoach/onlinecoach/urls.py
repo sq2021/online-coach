ad5e503990b9deefd4df607d2f084f62636117bf
TopKeingt <ppkhiemnguyen@gmail.com>
2019-05-26 10:01:40 -0400

    add url for question's editable endpoint

--- onlinecoach/onlinecoach/urls.py
+++ onlinecoach/onlinecoach/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', home_views.index, name='home-index')
+    path('', home_views.index, name='home-index'),
+    path('about', home_views.about, name='home-about')
 ]
 
 if settings.DEBUG:
