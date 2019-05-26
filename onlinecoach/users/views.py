5a9e0e759ba47e0c78bb203527f850a8f218e5a7
TopKeingt <ppkhiemnguyen@gmail.com>
2019-05-26 06:51:44 -0400

    update users app

--- onlinecoach/users/views.py
+++ onlinecoach/users/views.py
@@ -3,17 +3,19 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse 
+
+from .forms import UserRegisterForm
     
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             return HttpResponseRedirect(reverse('login'))
     else: 
-        form = UserCreationForm()
+        form = UserRegisterForm()
         
     return render(request, 'users/register.html', {'form': form})
 
