11c818b71b5018c34ad3b9611c2c4dadbf201cbd
TopKeingt <ppkhiemnguyen@gmail.com>
2019-05-26 06:21:48 -0400

    extend users model

--- onlinecoach/users/views.py
+++ onlinecoach/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse 
-
+    
 # Create your views here.
 def register(request):
     if request.method == 'POST':
